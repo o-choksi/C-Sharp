efd113116e2b439ca9e2f65001af9a830a235a40
Om Choksi <76880862+o-choksi@users.noreply.github.com>
2024-08-27 15:11:24 -0400

    Update NUnitProgram.cs

--- NUnit/source/NUnitProgram.cs
+++ NUnit/source/NUnitProgram.cs
@@ -1,1 +1,13 @@
+using UserInput;
 
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputHandler = new UserInputHandler();
+            inputHandler.ReadInputAndWriteToFile("output.txt");
+        }
+    }
+}
