a39fb289a24dda99138a34f92d6e4c955b9cd3ad
Om Choksi <76880862+o-choksi@users.noreply.github.com>
2024-08-27 00:57:12 -0400

    Update RuleService.cs

--- ProgressCorticon/RuleService.cs
+++ ProgressCorticon/RuleService.cs
@@ -3,7 +3,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
-using CorticonAPI;
+using CorticonAPI; //Need CorticonAPI.dll added to csproj
 using Microsoft.Extensions.Configuration;
 
 namespace ProgressCorticonIntegration
