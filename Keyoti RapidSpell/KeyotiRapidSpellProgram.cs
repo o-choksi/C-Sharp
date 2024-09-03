88e0a4953c0b3b7a6b99d00f529340bef985c43d
Om Choksi <76880862+o-choksi@users.noreply.github.com>
2024-09-03 00:47:38 -0400

    Update and rename KeyootiRapidSpellProgram.cs to KeyotiRapidSpellProgram.cs

--- Keyoti RapidSpell/KeyotiRapidSpellProgram.cs
+++ Keyoti RapidSpell/KeyotiRapidSpellProgram.cs
@@ -6,7 +6,7 @@ using Keyoti.RapidSpell.Dictionaries;
 
 namespace SpellCheckerApp
 {
-    class KeyootiRapidSpellProgram
+    class KeyotiRapidSpellProgram
     {
         static void Main(string[] args)
         {
